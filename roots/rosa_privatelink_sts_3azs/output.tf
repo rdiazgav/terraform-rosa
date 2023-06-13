ce9207ee0ec0d4c3ed0d67103b8612b2891a71ce
abel <apalma@apalma.remote.csb>
2023-06-13 18:21:29 +0200

    Enable port forwarding and add to output bastion PEM private key

--- roots/rosa_privatelink_sts_3azs/output.tf
+++ roots/rosa_privatelink_sts_3azs/output.tf
@@ -1,3 +1,8 @@
-output "ip_bastion" {
+output "bastion_ip" {
     value = "${aws_instance.egress-vpc-bastion.public_ip}"
+}
+
+output "bastion_private_key" {
+    value     = tls_private_key.ssh.private_key_pem
+    sensitive = true
 }
