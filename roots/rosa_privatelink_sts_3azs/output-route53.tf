7a6d2c201ca705265db8e32b1325b00eca6492c8
abel <apalma@apalma.remote.csb>
2023-06-14 09:28:17 +0200

    Add Terraform support for Route53 resolver in egress VPC

---
+++ roots/rosa_privatelink_sts_3azs/output-route53.tf
@@ -0,0 +1,3 @@
+output "rosa_private_zone_domain_UNDOCUMENTED" {
+    value     = "${ocm_cluster_rosa_classic.rosa.domain}"
+}
--- roots/rosa_privatelink_sts_3azs/output.tf
+++ roots/rosa_privatelink_sts_3azs/output.tf
@@ -2,6 +2,10 @@ output "bastion_ip" {
     value = "${aws_instance.egress-vpc-bastion.public_ip}"
 }
 
+output "private_bastion_ip" {
+    value= "${aws_instance.rosa-vpc-bastion.private_ip}"
+}
+
 output "bastion_private_key" {
     value     = tls_private_key.ssh.private_key_pem
     sensitive = true
