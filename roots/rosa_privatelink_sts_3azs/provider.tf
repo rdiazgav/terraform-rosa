1cceaaa5972a5e19a283834826fa4afaaf909ff0
Roberto Diaz Gavilanes <rdiazgav@redhat.com>
2023-11-10 17:11:39 +0100

    Update provider.tf

--- roots/rosa_privatelink_sts_3azs/provider.tf
+++ roots/rosa_privatelink_sts_3azs/provider.tf
@@ -1,25 +1,34 @@
 terraform {
-    required_version = ">= 0.12.0"
-    required_providers {
+  required_providers {
+    rhcs = {
+      version = "~> 1.2"
+      source  = "terraform-redhat/rhcs"
+    }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.20.0"
-    }
-    ocm = {
-      version = "= 1.0.4"
-      source  = "terraform-redhat/ocm"
+      version = "~> 4.0"
     }
   }
 }
 
-provider "aws" {
-	region = var.aws_region
-	ignore_tags {
-        key_prefixes = ["kubernetes.io/"]
-    }    
+provider "rhcs" {
+  token = var.offline_access_token
+  url   = var.url
 }
 
-provider "ocm" {
-  token = var.token
-  url   = var.url
+provider "aws" {
+  region = var.aws_region
+  # access_key = "${var.access_key}"
+  # secret_key = "${var.secret_key}"
+  profile = "default"
+
+  ignore_tags {
+    key_prefixes = ["kubernetes.io/"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "rhcs_rosa_operator_roles" "operator_roles" {
+  operator_role_prefix = var.cluster_name
 }
